4728281a9615df761261179827cf5c4346b722b2
Sasha-Solo <asolodilov@pugetsound.edu>
2023-03-27 02:39:51 +0000

    Finished hwk6

--- main.c
+++ main.c
@@ -4,17 +4,85 @@
 #include <stdlib.h>
 #include "ts_hashmap.h"
 
+/**
+* Globals
+**/
+ts_hashmap_t *map; //hashmap
+
+void *testRoutine(){
+	for (int i = 0; i < 5; i++){ //test loops 5 times
+		int action = (rand() % 3) + 1; //generate an action for threads to do
+
+		if (action == 1){ //do a put
+		    int r = rand() % 100; //get a random number
+			put(map, r, r);
+		}
+		else if (action == 2) { //do a get
+			int r = rand() % 100;
+			get(map, r);
+		}
+		else if (action == 3){ //do a del
+			int r = rand() % 100;
+       		del(map, r);
+		}
+	}
+	return NULL;
+}
+
 int main(int argc, char *argv[]) {
 	if (argc < 3) {
 		printf("Usage: %s <num threads> <hashmap capacity>\n", argv[0]);
 		return 1;
 	}
-
-  srand(time(NULL));
+	
+  	srand(time(NULL)); 
 	int num_threads = atoi(argv[1]);
 	int capacity = (unsigned int) atoi(argv[2]);
 
-	// TODO: Write your test
+	map = initmap(capacity);
+
+	// allocate space to hold threads
+  	pthread_t *threads = (pthread_t*) malloc(num_threads * sizeof(pthread_t));
+
+	for (int i = 0; i < num_threads; i++) { //spawn off threads
+    	pthread_create(&threads[i], NULL, testRoutine, NULL);
+  	}
+
+	for (int i = 0; i < num_threads; i++){ //join threads
+		pthread_join(threads[i], NULL);
+	}
+
+	printmap(map);
+
+	for (int i = 0; i < capacity; i++){
+		ts_entry_t *prev; //prev is for cycling through entries
+		ts_entry_t *ent; //the entry
+
+		ent = map->table[i];
+		
+		while (ent != NULL){
+			prev = ent; //prev and ent point to the same thing
+			ent = ent->next; //move ent along
+			free(prev); //free prev
+			prev = NULL; //prev set to NULL
+		}
+		map->table[i] = NULL;
+	}
+
+	free(map->table); //free up space of table
+	map->table = NULL; //defensive programming
+	free(map); //free up space for map
+	map = NULL; //defensive programming
+
+	free(threads); //free threads
+	threads = NULL;
+
+	for (int i = 0; i < capacity; i++){ //destroy mutexes
+		pthread_mutex_destroy(&locks[i]);
+	}
+
+	free(locks); //free locks array
+	locks = NULL;
 
 	return 0;
 }
--- ts_hashmap.c
+++ ts_hashmap.c
@@ -6,14 +6,40 @@
 #include "ts_hashmap.h"
 
 /**
+* Globals
+**/
+pthread_mutex_t *locks; //array of locks
+
+/**
  * Creates a new thread-safe hashmap. 
  *
  * @param capacity initial capacity of the hashmap.
  * @return a pointer to a new thread-safe hashmap.
  */
 ts_hashmap_t *initmap(int capacity) {
-  // TODO
-  return NULL;
+  
+   //malloc space  
+   ts_hashmap_t *initial = (ts_hashmap_t*) malloc (1 * sizeof(ts_hashmap_t));
+
+   //initialize args inside the struct
+   initial->capacity = capacity;  //initialize capacity 
+   initial->size = 0; //set size to 0 because no entry is stored yet
+
+   //malloc space for table
+   initial->table = (ts_entry_t**) malloc(capacity * sizeof(ts_entry_t*));
+  
+   for (int i = 0; i < capacity; i++) { //set all slots to null initially
+     initial->table[i] = NULL; 
+   }
+
+   //get space for array to hold locks
+	 locks = (pthread_mutex_t*) malloc (sizeof(pthread_mutex_t) * capacity);
+
+	 for (int i = 0; i < capacity; i++){ //initialize mutexes inside locks array
+		 pthread_mutex_init(&locks[i], NULL);
+	 }
+  
+   return initial; 
 }
 
 /**
@@ -23,8 +49,31 @@ ts_hashmap_t *initmap(int capacity) {
  * @return the value associated with the given key, or INT_MAX if key not found
  */
 int get(ts_hashmap_t *map, int key) {
-  // TODO
-  return INT_MAX;
+   // TODO
+   unsigned int slot = (unsigned int) key; //cast key as unsigned int
+   int index = slot % (map->capacity); //get index 
+
+   pthread_mutex_lock(&locks[index]); //lock row with a lock at given index
+
+   ts_entry_t *entry = map->table[index]; //get given entry
+
+   if (entry == NULL){ //if entry is null, return INT_MAX
+     pthread_mutex_unlock(&locks[index]); //unlock row
+     return INT_MAX;
+   }
+   else{
+    ts_entry_t *prev;
+    while (entry != NULL){ //if entry exists...
+      prev = entry;
+      if (key == entry->key){ //check if given key matches entry key
+        pthread_mutex_unlock(&locks[index]); //unlock row
+        return entry->value;
+      }
+      entry = prev->next;
+    }
+   }
+   pthread_mutex_unlock(&locks[index]); //unlock row
+   return INT_MAX;
 }
 
 /**
@@ -35,9 +84,55 @@ int get(ts_hashmap_t *map, int key) {
  * @return old associated value, or INT_MAX if the key was new
  */
 int put(ts_hashmap_t *map, int key, int value) {
-  // TODO
+  //TODO
+  unsigned int slot = (unsigned int) key; //cast key as unsigned int
+  int index = slot % (map->capacity); //get index 
+
+  pthread_mutex_lock(&locks[index]); //lock row with a lock at given index
+
+  ts_entry_t *entry = map->table[index]; //get given entry
+  
+  if (entry == NULL){ //if entry is empty...
+    //printf("\nentry is null\n");
+     //get space for ts_entry_t struct
+     entry = (ts_entry_t*) malloc (1 * sizeof(ts_entry_t)); 
+     entry->key = key;  //initialize key with what was passed in 
+     entry->value = value; //initialize value with what was passed in 
+     entry->next = NULL; //set next pointer to null
+     map->table[index] = entry; //insert the entry
+     map->size++; //increment size
+
+     pthread_mutex_unlock(&locks[index]); //unlock row
+     return INT_MAX;
+   }
+
+   ts_entry_t *prev; //will be used to move along entries during while loop
+   int oldValue = 0;
+   while (entry != NULL){ //if entry is full...
+      if (key == entry->key){ //check if key passed in matches key from an entry
+       oldValue = entry->value; //store the old value
+       entry->value = value; //replace the value with the value passed in 
+
+       pthread_mutex_unlock(&locks[index]); //unlock row
+       return oldValue; //return the old value
+      }
+
+     prev = entry; //set prev to current entry
+     entry = prev->next; //move onto the next entry
+   }
+   
+   //match not found, and haven't reached size yet so malloc space for new entry
+   entry = (ts_entry_t*) malloc (1 * sizeof(ts_entry_t));  
+   entry->key = key;  //sets key for new entry
+   entry->value = value; //sets value for new entry
+   entry->next = NULL; //set next pointer to null of new entry
+   prev->next = entry; //insert the entry
+   map->size++; //increment size
+
+  pthread_mutex_unlock(&locks[index]); //unlock row
   return INT_MAX;
 }
+
 
 /**
  * Removes an entry in the map
@@ -46,8 +141,66 @@ int put(ts_hashmap_t *map, int key, int value) {
  * @return the value associated with the given key, or INT_MAX if key not found
  */
 int del(ts_hashmap_t *map, int key) {
+
   // TODO
-  return INT_MAX;
+   unsigned int slot = (unsigned int) key; //cast key as unsigned int
+   int index = slot % (map->capacity); //get index 
+
+   pthread_mutex_lock(&locks[index]); //unlock row
+
+   ts_entry_t *entry = map->table[index]; //get given entry
+    int oldValue = 0;
+   if (entry == NULL){ //if entry is null, return INT_MAX
+     pthread_mutex_unlock(&locks[index]); //unlock row
+     return INT_MAX;
+   }
+   else{
+    ts_entry_t *prev;
+    //ts_entry_t *temp;
+    if (entry->next == NULL && key == entry->key){ //if this is the first and only element
+      oldValue = entry->value; //store the value to return later
+      free(entry); //free entry
+      entry = NULL; //set to null
+      map->table[index] = NULL;
+      map->size--; //decrement size after deletion
+      
+      pthread_mutex_unlock(&locks[index]); //unlock row
+      return oldValue; //return the old value
+    }
+
+    int firstItem = 1; //to keep track of the first element
+    
+    while (entry != NULL){ //if entry exists...
+      if (key == entry->key){ //check if given key matches entry key
+          if (firstItem == 1){ //if this is the first thing in list and there are elements after it
+            oldValue = entry->value; //store the value to return later
+            ts_entry_t *temp = entry->next;
+            free(entry); //free
+            entry = NULL; //set to null
+            map->table[index] = temp;
+            map->size--; //decrement size after deletion
+
+            pthread_mutex_unlock(&locks[index]); //unlock row
+            return oldValue;
+          }
+          else{ //the element to be deleted is in the middle of the list
+            oldValue = entry->value; //store the value to return later
+            prev->next = entry->next; //setting previous elements' next val to the element after deleted element
+            free(entry); //free
+            entry = NULL; //set to null
+            map->size--; //decrement size after deletion
+
+            pthread_mutex_unlock(&locks[index]); //unlock row
+            return oldValue; //return the old value
+          }
+      }
+      prev = entry;
+      entry = prev->next;
+      firstItem++;
+    }
+   }
+   pthread_mutex_unlock(&locks[index]); //unlock row
+   return INT_MAX;
 }
 
 
